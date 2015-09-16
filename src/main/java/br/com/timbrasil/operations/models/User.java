6e0137b5762c8951436cf79891b2c5730c5552b5
Pedro Rabello <pedrommrabello@gmail.com>
2015-09-16 12:40:01 -0300

    Adicionado indexes ao register e email da classe User

--- src/main/java/br/com/timbrasil/operations/models/User.java
+++ src/main/java/br/com/timbrasil/operations/models/User.java
@@ -20,7 +20,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 @Entity
 @Table(name = "\"User\"", indexes = {
 		@Index(name = "email_index", columnList = "email"),
-		@Index(name = "register", columnList = "register") })
+		@Index(name = "register_index", columnList = "register") })
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
