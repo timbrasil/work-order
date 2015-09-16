2e6e985a485dbcb27650722c6a3a107b84c1b6b7
Pedro Rabello <pedrommrabello@gmail.com>
2015-09-16 10:01:42 -0300

    User serializado

--- src/main/java/br/com/timbrasil/operations/models/User.java
+++ src/main/java/br/com/timbrasil/operations/models/User.java
@@ -1,5 +1,7 @@
 package br.com.timbrasil.operations.models;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -15,7 +17,9 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Table(name = "\"User\"")
-public class User {
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
