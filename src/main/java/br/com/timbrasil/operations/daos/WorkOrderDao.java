62f3123a96bf19e85af84dedb82241effc925a92
Pedro Rabello <pedrommrabello@gmail.com>
2015-09-18 10:38:26 -0300

    WorkOrder dao criado

---
+++ src/main/java/br/com/timbrasil/operations/daos/WorkOrderDao.java
@@ -0,0 +1,34 @@
+package br.com.timbrasil.operations.daos;
+
+import javax.enterprise.context.RequestScoped;
+import javax.persistence.EntityManager;
+
+import br.com.timbrasil.operations.models.WorkOrder;
+
+@RequestScoped
+public class WorkOrderDao {
+	
+	private final EntityManager manager;
+
+	public WorkOrderDao(EntityManager manager) {
+		this.manager = manager;
+	}
+	
+	/**
+	 * CDI
+	 */
+	@Deprecated
+	public WorkOrderDao(){
+		this(null);
+	}
+	
+	public void save(WorkOrder workOrder){
+		manager.persist(workOrder);
+	}
+	
+	public WorkOrder find(long id){
+		return manager.find(WorkOrder.class, id);
+	}
+	
+
+}
